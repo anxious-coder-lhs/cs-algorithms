0c0b3a39007651c4066505a364db8306a3342358
Love Hasija <love2dishtech@gmail.com>
2020-03-08 21:01:12 +0530

    Update LeetCode_665_Easy_BF.js

--- LeetCode_665_Easy_BF.js
+++ LeetCode_665_Easy_BF.js
@@ -6,8 +6,36 @@
 // Array is unsorted.
 // Find if the array is sorted in ascending order if we just modify 1 element.
 
-// Complexity: O(n*n) runtime, O(1) space.
+// Complexity: O(n) runtime, O(1) space.
 var checkPossibility = function(nums) {
+    // Solution Approach Pattern - Minimizing the noise in the problem by breaking
+    // the problem into smaller problem. If the problem can be broken down into a 
+    // small problem, it can be easy to fix or approach.
+
+    // Moving from left most to the right, till we find a problem (Problem index from left)
+    // Moving from right most to the left, till we find a problem (Problem index from right)
+    // If the problem indexes do not exist, bypass each other - there is no problem.
+        // have a distance of < 2, there is nothing to solve. "2" because the window size is 3 starting from 2nd index.
+    // If the problem indexes are less than 4 distance apart, one BF scan can fix/check it.
+    // If it is greater than 4, there are 2 separate problems, that need more than one scan.
+    
+    // Scanning with a window of 3 to find the 2 problem indexes.
+    let i=0, j=nums.length - 1
+    while(i+2<nums.length && nums[i] <= nums[i+1] && nums[i+1] <= nums[i+2]) i++
+    while(j-2>=0 && nums[j-2] <= nums[j-1] && nums[j-1] <= nums[j]) j--
+
+    // No problem exists
+    if (j - i + 1 <= 2) return true
+
+    // 2 distinct problems
+    if (j-i+1 >= 5) return false
+
+    // Try BF to see if this can be fixed. O(1) for constant size of 4.
+    return checkPossibilityBF(nums.slice(i, j+1))
+};
+
+// Complexity: O(n*n) runtime, O(1) space.
+var checkPossibilityBF = function(nums) {
     // Brute force solution by trying all the possibilities for making a change.
     // Best case solution assumes that there is no change required.
     // Another possible solution requires that there is only one change.
