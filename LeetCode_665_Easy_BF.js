d2b2e9a7b6db0da09e90692a034c1a05d7545143
Love Hasija <love2dishtech@gmail.com>
2020-03-08 16:32:43 +0530

    Update LeetCode_665_Easy_BF.js

--- LeetCode_665_Easy_BF.js
+++ LeetCode_665_Easy_BF.js
@@ -5,30 +5,34 @@
 // Given an array of length n - n >= 1 && <= 10,000
 // Array is unsorted.
 // Find if the array is sorted in ascending order if we just modify 1 element.
-var checkPossibility = function(nums, outOfOrder=0) {
-// Brute force
-// Iterate through the array for each pair.
-// If we find an out of order.
-    // Change item 1 and run through the loop
-    // Change item 2 and run through the loop
-    
-    for (let i=0;i<nums.length-1;i++) {
-        if (nums[i] > nums[i+1]) {
-            // Found out of order items.
-            outOfOrder++
-            if (outOfOrder >= 2) {
-                return false
-            } else {
-                const first = nums[i]
-                const second = nums[i+1]
-                const newOne = nums.slice()
-                newOne.splice(i, 1, second)
-                const newTwo = nums.slice()
-                newTwo.splice(i+1, 1, first)
-                return checkPossibility(newOne, outOfOrder) || checkPossibility(newTwo, outOfOrder)
-            }
-        }
-    }
-    
-    return true
+var checkPossibility = function(nums) {
+    // Brute force solution by trying all the possibilities for making a change.
+    // Best case solution assumes that there is no change required.
+    // Another possible solution requires that there is only one change.
+        // One change means that only a single element has to change.
+        // Only single element that ensures result is making A[i] to A[i-1]
+        // Making A[i] to -Infinity in case i is 0.
+
+   // A BF solution will try making all the possible changes and looking for result.
+
+   for (let i=0;i<nums.length;i++) {
+       const original = nums[i]
+       nums[i] = i === 0 ? -Infinity : nums[i-1]
+       if (checkSeqValid(nums)) {
+           return true
+       }
+       nums[i] = original
+   }
+
+   return false
 };
+
+function checkSeqValid(nums) {
+    let last = -Infinity
+    for (const num of nums) {
+        if (num < last)
+            return false
+        last = num
+    }
+    return true
+}
