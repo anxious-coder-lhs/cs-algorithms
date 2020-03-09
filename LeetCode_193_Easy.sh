b7bb49520bb31fab895d0118dbce5b6dac3b98b6
Love Hasija <love2dishtech@gmail.com>
2020-03-09 19:49:06 +0530

    Create LeetCode_193_Easy.sh

---
+++ LeetCode_193_Easy.sh
@@ -0,0 +1,4 @@
+# Extracing valid phone numbers from the file where each line contains possibily a valid US phone number.
+
+# Read from the file file.txt and output all valid phone numbers to stdout.
+grep -E "^\([0-9]{3}\) [0-9]{3}-[0-9]{4}$|^[0-9]{3}-[0-9]{3}-[0-9]{4}$" file.txt
