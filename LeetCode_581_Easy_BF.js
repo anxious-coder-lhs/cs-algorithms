823fb1e4c2046bd30a65ac4a22cc9c70a21d5127
Love Hasija <love2dishtech@gmail.com>
2020-03-18 16:32:54 +0530

    Update LeetCode_581_Easy_BF.js

--- LeetCode_581_Easy_BF.js
+++ LeetCode_581_Easy_BF.js
@@ -6,7 +6,7 @@
 // Outout: Minimum Continuous subarray length
 // Condition: If subarray is sorted, then the entire array would be sorted.
 
-// O(n log n) run-time, O(1) space time
+// O(n * n) run-time, O(1) space time
 var findUnsortedSubarray = function(nums) {
     
     // Brute force solution:
@@ -14,27 +14,56 @@ var findUnsortedSubarray = function(nums) {
     // Since we are looking for the shortest subarray as the result, we should expand the brute force array such that we increase the length of it.
     // For each shortest subarray, sort the subarray and check if the entire array is now sorted.
     
+    // In the brute force solution, we are exploiting few properties of the solution state:
+        // 1. Left subarray would be sorted.
+        // 2. Right subarray would be sorted.
+        // 3. Min of center subarray is >= last element of left.
+        // 4. Max of center subarray is <= 1st element of right.
+    
     if (nums.length <= 1 || isSorted(nums)) {
         return 0
     }
     
     for (let windowSize=2;windowSize<=nums.length;windowSize++) {
         for (let leftIdx=0;leftIdx<=nums.length - windowSize;leftIdx++) {
-            const sortedSubarray = nums.slice(leftIdx, leftIdx + windowSize).sort((a, b) => a-b)
-            const sortedArray = nums.slice(0, leftIdx).concat(sortedSubarray.concat(nums.slice(leftIdx + windowSize)))
-            // console.log(sortedArray)
-            if (isSorted(sortedArray)) {
+            const rightIdx = leftIdx + windowSize
+            const [min, max] = findMinMax(nums, leftIdx, rightIdx)
+//             console.log(leftIdx, rightIdx, min, max)
+            if (isSorted(nums, 0, leftIdx) && 
+                isSorted(nums, rightIdx, nums.length) && 
+                (leftIdx === 0 ? true : nums[leftIdx-1] <= min) && 
+                (rightIdx === nums.length ? true : nums[rightIdx] >= max)) {
                 return windowSize
             }
         }    
     }
+    
+    return nums.length
 };
 
-function isSorted(nums) {
-    let last = -Infinity
-    for (const num of nums) {
-        if (num < last) return false
-        last = num
+// Finding the min and the max number from a given array.
+function findMinMax(nums, start, end) {
+    let min = nums[start]
+    let max = nums[start]
+    for (let i=start+1;i<end;i++) {
+        if (nums[i] < min) {
+            min = nums[i]
+        }
+        
+        if (nums[i] > max) {
+            max = nums[i]
+        }
+    }
+    
+    return [min, max]
+}
+
+// Find if the array is sorted
+function isSorted(nums, start = 0, end = nums.length) {
+    let last = nums[start]
+    for (let i=start+1;i<end;i++) {
+        if (nums[i] < last) return false
+        last = nums[i]
     }
     
     return true
